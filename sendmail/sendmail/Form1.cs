af229bfbe5cfa16bfbe6092125bf5f0683f3c91e
o0Poseidon0o <90851953+o0Poseidon0o@users.noreply.github.com>
2021-10-30 21:01:33 +0700

    Form1.cs
    nhan up lai

--- sendmail/sendmail/Form1.cs
+++ sendmail/sendmail/Form1.cs
@@ -4,22 +4,32 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+
+using System.Net.Mail;
+using System.Net;
+using System.IO;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace sendmail
 {
     public partial class Form1 : Form
     {
+        Attachment attack = null;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void btnAttack_Click(object sender, EventArgs e)
         {
-
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                txbAttack.Text = dialog.FileName;
+            }
         }
     }
 }
+
