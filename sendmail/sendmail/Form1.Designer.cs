1804a53c80170625e9e41fbc0b068a8b98112886
dinhduyanh01 <54561476+dinhduyanh01@users.noreply.github.com>
2021-10-30 22:05:06 +0700

    form1.cs
    sua loi

--- sendmail/sendmail/Form1.Designer.cs
+++ sendmail/sendmail/Form1.Designer.cs
@@ -29,215 +29,203 @@ namespace sendmail
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblfile = new System.Windows.Forms.Label();
+            this.lblTo = new System.Windows.Forms.Label();
+            this.lblSubject = new System.Windows.Forms.Label();
+            this.lblId = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
             this.txbAttack = new System.Windows.Forms.TextBox();
             this.btnAttack = new System.Windows.Forms.Button();
-            this.lblTo = new System.Windows.Forms.Label();
             this.txbTo = new System.Windows.Forms.TextBox();
-            this.btnDanhSach = new System.Windows.Forms.Button();
-            this.lblSubject = new System.Windows.Forms.Label();
             this.txbSubject = new System.Windows.Forms.TextBox();
-            this.lblId = new System.Windows.Forms.Label();
             this.txbId = new System.Windows.Forms.TextBox();
-            this.lblPassword = new System.Windows.Forms.Label();
             this.txbPassword = new System.Windows.Forms.TextBox();
-            this.lblMessage = new System.Windows.Forms.Label();
             this.btnSent = new System.Windows.Forms.Button();
             this.txbMessage = new System.Windows.Forms.TextBox();
+            this.btnDanhSach = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.lblfile = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // lblfile
-            // 
-            this.lblfile.AutoSize = true;
-            this.lblfile.Location = new System.Drawing.Point(29, 32);
-            this.lblfile.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblfile.Name = "lblfile";
-            this.lblfile.Size = new System.Drawing.Size(38, 17);
-            this.lblfile.TabIndex = 11;
-            this.lblfile.Text = "File :";
-            // 
-            // txbAttack
-            // 
-            this.txbAttack.Location = new System.Drawing.Point(150, 32);
-            this.txbAttack.Margin = new System.Windows.Forms.Padding(4);
-            this.txbAttack.Name = "txbAttack";
-            this.txbAttack.ReadOnly = true;
-            this.txbAttack.Size = new System.Drawing.Size(405, 22);
-            this.txbAttack.TabIndex = 12;
-            this.txbAttack.TabStop = false;
-            // 
-            // btnAttack
-            // 
-            this.btnAttack.Location = new System.Drawing.Point(621, 32);
-            this.btnAttack.Margin = new System.Windows.Forms.Padding(4);
-            this.btnAttack.Name = "btnAttack";
-            this.btnAttack.Size = new System.Drawing.Size(111, 28);
-            this.btnAttack.TabIndex = 13;
-            this.btnAttack.Text = "Attack";
-            this.btnAttack.UseVisualStyleBackColor = true;
             // 
             // lblTo
             // 
             this.lblTo.AutoSize = true;
-            this.lblTo.Location = new System.Drawing.Point(34, 97);
-            this.lblTo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTo.Location = new System.Drawing.Point(23, 52);
             this.lblTo.Name = "lblTo";
-            this.lblTo.Size = new System.Drawing.Size(33, 17);
-            this.lblTo.TabIndex = 14;
+            this.lblTo.Size = new System.Drawing.Size(26, 13);
+            this.lblTo.TabIndex = 0;
             this.lblTo.Text = "To :";
-            // 
-            // txbTo
-            // 
-            this.txbTo.Location = new System.Drawing.Point(152, 92);
-            this.txbTo.Margin = new System.Windows.Forms.Padding(4);
-            this.txbTo.Name = "txbTo";
-            this.txbTo.ReadOnly = true;
-            this.txbTo.Size = new System.Drawing.Size(405, 22);
-            this.txbTo.TabIndex = 15;
-            // 
-            // btnDanhSach
-            // 
-            this.btnDanhSach.Location = new System.Drawing.Point(621, 92);
-            this.btnDanhSach.Margin = new System.Windows.Forms.Padding(4);
-            this.btnDanhSach.Name = "btnDanhSach";
-            this.btnDanhSach.Size = new System.Drawing.Size(115, 28);
-            this.btnDanhSach.TabIndex = 16;
-            this.btnDanhSach.Text = "Mail List";
-            this.btnDanhSach.UseVisualStyleBackColor = true;
             // 
             // lblSubject
             // 
             this.lblSubject.AutoSize = true;
-            this.lblSubject.Location = new System.Drawing.Point(29, 170);
-            this.lblSubject.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSubject.Location = new System.Drawing.Point(23, 87);
             this.lblSubject.Name = "lblSubject";
-            this.lblSubject.Size = new System.Drawing.Size(63, 17);
-            this.lblSubject.TabIndex = 17;
+            this.lblSubject.Size = new System.Drawing.Size(49, 13);
+            this.lblSubject.TabIndex = 0;
             this.lblSubject.Text = "Subject :";
-            // 
-            // txbSubject
-            // 
-            this.txbSubject.Location = new System.Drawing.Point(150, 170);
-            this.txbSubject.Margin = new System.Windows.Forms.Padding(4);
-            this.txbSubject.Name = "txbSubject";
-            this.txbSubject.Size = new System.Drawing.Size(407, 22);
-            this.txbSubject.TabIndex = 18;
             // 
             // lblId
             // 
             this.lblId.AutoSize = true;
-            this.lblId.Location = new System.Drawing.Point(29, 236);
-            this.lblId.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblId.Location = new System.Drawing.Point(23, 125);
             this.lblId.Name = "lblId";
-            this.lblId.Size = new System.Drawing.Size(113, 17);
-            this.lblId.TabIndex = 19;
+            this.lblId.Size = new System.Drawing.Size(87, 13);
+            this.lblId.TabIndex = 0;
             this.lblId.Text = "Tên đăng nhập :";
-            // 
-            // txbId
-            // 
-            this.txbId.Location = new System.Drawing.Point(150, 236);
-            this.txbId.Margin = new System.Windows.Forms.Padding(4);
-            this.txbId.Name = "txbId";
-            this.txbId.Size = new System.Drawing.Size(407, 22);
-            this.txbId.TabIndex = 20;
-            // 
-            // lblPassword
-            // 
-            this.lblPassword.AutoSize = true;
-            this.lblPassword.Location = new System.Drawing.Point(34, 293);
-            this.lblPassword.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblPassword.Name = "lblPassword";
-            this.lblPassword.Size = new System.Drawing.Size(74, 17);
-            this.lblPassword.TabIndex = 21;
-            this.lblPassword.Text = "Mật khẩu :";
-            // 
-            // txbPassword
-            // 
-            this.txbPassword.Location = new System.Drawing.Point(148, 290);
-            this.txbPassword.Margin = new System.Windows.Forms.Padding(4);
-            this.txbPassword.Name = "txbPassword";
-            this.txbPassword.Size = new System.Drawing.Size(407, 22);
-            this.txbPassword.TabIndex = 22;
-            this.txbPassword.UseSystemPasswordChar = true;
             // 
             // lblMessage
             // 
             this.lblMessage.AutoSize = true;
-            this.lblMessage.Location = new System.Drawing.Point(34, 354);
-            this.lblMessage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMessage.Location = new System.Drawing.Point(23, 198);
             this.lblMessage.Name = "lblMessage";
-            this.lblMessage.Size = new System.Drawing.Size(65, 17);
-            this.lblMessage.TabIndex = 23;
+            this.lblMessage.Size = new System.Drawing.Size(50, 13);
+            this.lblMessage.TabIndex = 0;
             this.lblMessage.Text = "Message";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(23, 160);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(58, 13);
+            this.lblPassword.TabIndex = 0;
+            this.lblPassword.Text = "Mật khẩu :";
+            // 
+            // txbAttack
+            // 
+            this.txbAttack.Location = new System.Drawing.Point(117, 12);
+            this.txbAttack.Name = "txbAttack";
+            this.txbAttack.ReadOnly = true;
+            this.txbAttack.Size = new System.Drawing.Size(305, 20);
+            this.txbAttack.TabIndex = 1;
+            this.txbAttack.TabStop = false;
+            // 
+            // btnAttack
+            // 
+            this.btnAttack.Location = new System.Drawing.Point(433, 12);
+            this.btnAttack.Name = "btnAttack";
+            this.btnAttack.Size = new System.Drawing.Size(83, 23);
+            this.btnAttack.TabIndex = 5;
+            this.btnAttack.Text = "Attack";
+            this.btnAttack.UseVisualStyleBackColor = true;
+            this.btnAttack.Click += new System.EventHandler(this.btnAttack_Click);
+            // 
+            // txbTo
+            // 
+            this.txbTo.Location = new System.Drawing.Point(117, 52);
+            this.txbTo.Name = "txbTo";
+            this.txbTo.ReadOnly = true;
+            this.txbTo.Size = new System.Drawing.Size(306, 20);
+            this.txbTo.TabIndex = 0;
+            // 
+            // txbSubject
+            // 
+            this.txbSubject.Location = new System.Drawing.Point(117, 84);
+            this.txbSubject.Name = "txbSubject";
+            this.txbSubject.Size = new System.Drawing.Size(306, 20);
+            this.txbSubject.TabIndex = 1;
+            // 
+            // txbId
+            // 
+            this.txbId.Location = new System.Drawing.Point(116, 118);
+            this.txbId.Name = "txbId";
+            this.txbId.Size = new System.Drawing.Size(306, 20);
+            this.txbId.TabIndex = 2;
+            // 
+            // txbPassword
+            // 
+            this.txbPassword.Location = new System.Drawing.Point(116, 153);
+            this.txbPassword.Name = "txbPassword";
+            this.txbPassword.Size = new System.Drawing.Size(306, 20);
+            this.txbPassword.TabIndex = 3;
+            this.txbPassword.UseSystemPasswordChar = true;
             // 
             // btnSent
             // 
-            this.btnSent.Location = new System.Drawing.Point(152, 348);
-            this.btnSent.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSent.Location = new System.Drawing.Point(116, 198);
             this.btnSent.Name = "btnSent";
-            this.btnSent.Size = new System.Drawing.Size(100, 28);
-            this.btnSent.TabIndex = 24;
+            this.btnSent.Size = new System.Drawing.Size(75, 23);
+            this.btnSent.TabIndex = 6;
             this.btnSent.Text = "Sent";
             this.btnSent.UseVisualStyleBackColor = true;
+            this.btnSent.Click += new System.EventHandler(this.btnSent_Click);
             // 
             // txbMessage
             // 
-            this.txbMessage.Location = new System.Drawing.Point(32, 408);
-            this.txbMessage.Margin = new System.Windows.Forms.Padding(4);
+            this.txbMessage.Location = new System.Drawing.Point(26, 227);
             this.txbMessage.Multiline = true;
             this.txbMessage.Name = "txbMessage";
-            this.txbMessage.Size = new System.Drawing.Size(759, 151);
-            this.txbMessage.TabIndex = 25;
+            this.txbMessage.Size = new System.Drawing.Size(490, 170);
+            this.txbMessage.TabIndex = 4;
+            // 
+            // btnDanhSach
+            // 
+            this.btnDanhSach.Location = new System.Drawing.Point(430, 52);
+            this.btnDanhSach.Name = "btnDanhSach";
+            this.btnDanhSach.Size = new System.Drawing.Size(86, 23);
+            this.btnDanhSach.TabIndex = 7;
+            this.btnDanhSach.Text = "Mail List";
+            this.btnDanhSach.UseVisualStyleBackColor = true;
+            this.btnDanhSach.Click += new System.EventHandler(this.btnDanhSach_Click);
             // 
             // textBox1
             // 
             this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
-            this.textBox1.Location = new System.Drawing.Point(37, 621);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Location = new System.Drawing.Point(26, 403);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(419, 22);
-            this.textBox1.TabIndex = 26;
+            this.textBox1.Size = new System.Drawing.Size(315, 20);
+            this.textBox1.TabIndex = 8;
             this.textBox1.Text = "https://www.google.com/settings/u/1/security/lesssecureapps";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Red;
-            this.label1.Location = new System.Drawing.Point(493, 626);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(348, 409);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(243, 17);
-            this.label1.TabIndex = 27;
+            this.label1.Size = new System.Drawing.Size(185, 13);
+            this.label1.TabIndex = 9;
             this.label1.Text = "cho phép để sử dụng theo dường link";
+            // 
+            // lblfile
+            // 
+            this.lblfile.AutoSize = true;
+            this.lblfile.Location = new System.Drawing.Point(23, 12);
+            this.lblfile.Name = "lblfile";
+            this.lblfile.Size = new System.Drawing.Size(29, 13);
+            this.lblfile.TabIndex = 10;
+            this.lblfile.Text = "File :";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(822, 690);
+            this.ClientSize = new System.Drawing.Size(537, 433);
+            this.Controls.Add(this.lblfile);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.txbMessage);
-            this.Controls.Add(this.btnSent);
-            this.Controls.Add(this.lblMessage);
-            this.Controls.Add(this.txbPassword);
-            this.Controls.Add(this.lblPassword);
-            this.Controls.Add(this.txbId);
-            this.Controls.Add(this.lblId);
-            this.Controls.Add(this.txbSubject);
-            this.Controls.Add(this.lblSubject);
             this.Controls.Add(this.btnDanhSach);
+            this.Controls.Add(this.btnSent);
+            this.Controls.Add(this.txbMessage);
+            this.Controls.Add(this.txbPassword);
+            this.Controls.Add(this.txbId);
+            this.Controls.Add(this.txbSubject);
             this.Controls.Add(this.txbTo);
-            this.Controls.Add(this.lblTo);
             this.Controls.Add(this.btnAttack);
             this.Controls.Add(this.txbAttack);
-            this.Controls.Add(this.lblfile);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.lblSubject);
+            this.Controls.Add(this.lblTo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
